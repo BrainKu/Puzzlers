4d8b9f2c5e762300ed56084961d69a6636504f52
Ku <kuxinwei@163.com>
2016-12-23 08:31:01 +0800

    tmp commit

--- src/interview/chap8/Section1.java
+++ src/interview/chap8/Section1.java
@@ -6,7 +6,8 @@ package interview.chap8;
 public class Section1 {
 
     public static void main(String[] args) {
-        test4();
+//        test4();
+        test5();
     }
 
     private static void test4() {
@@ -35,5 +36,48 @@ public class Section1 {
         inputs[index+1] = '2';
         inputs[index+2] = '0';
         return length + 2;
+    }
+
+    public static void test5() {
+        String input = "aabcccccaaab"; // 如果超过10位该怎么办?
+        String output = zipString(input);
+        System.out.println(output);
+    }
+
+
+    private static String zipString(String input) {
+        if (input == null || input.length() == 1) return input;
+        char[] inputs = input.toCharArray();
+        int count = 1;
+        StringBuilder sb = new StringBuilder(inputs[0]+""); //  这里也是个坑啊
+        for (int i = 1, length = inputs.length; i < length; i++) {
+
+        }
+        return sb.toString();
+    }
+
+    private static String zipString0(String input) {
+        if (input == null || input.length() == 1) return input;
+        char[] inputs = input.toCharArray();
+        int count = 1;
+        int end = 0;
+        for (int i = 1, length = inputs.length; i < length; i++) {
+            if (inputs[i-1] == inputs[i]) {
+                count++;
+            } else {
+                if (count == 1) {
+                    inputs[++end] = inputs[i];
+                } else {
+                    System.out.println(inputs[i-1]);
+                    System.out.println(count);
+                    inputs[++end] = (char) ('0' + count);
+                    count = 1;
+                }
+            }
+        }
+        if (end == inputs.length - 1) {
+            return input;
+        }
+        return new String(inputs).substring(0, end);
     }
 }
